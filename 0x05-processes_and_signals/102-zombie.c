9bbed519f0944972f3fa969784b7a34de5db63bb
Gizachew Bayness <eleccrazy24@gmail.com>
2022-08-19 13:46:15 -0400

    task 12

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,41 @@
+/**
+ * infinite_while - Just runs infinite while loop.
+ *
+ * Return: 0
+ *
+ */
+
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+
+/**
+ * main - creates processes
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	pid_t pid;
+	int i = 0;
+
+	for (; i < 5; i++)
+	{
+		pid = fork();
+		if (pid > 0)
+			printf("Zombie process created, PID: %d\n", pid);
+		else
+			return (0);
+	}
+
+	infinite_while();
+
+	return (0)
+}
