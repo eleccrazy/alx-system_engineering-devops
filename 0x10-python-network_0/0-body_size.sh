b96bc293c2b0b117a484b0591bb2847c40398033
Gizachew Bayness <83591565+eleccrazy@users.noreply.github.com>
2022-10-20 18:39:07 +0300

    updated

--- 0x10-python-network_0/0-body_size.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-# This Bash Script  takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
--- 0x10-python-network_0/README.md
+++
@@ -1,1 +0,0 @@
-This is 0x010-python-network_0 project in the ALX 12 month software engineering program
