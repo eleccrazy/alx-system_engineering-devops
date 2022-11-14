2209e6df7b1932f90685d398610bc07e69870e19
Gizachew Bayness <eleccrazy24@gmail.com>
2022-11-14 17:28:12 +0300

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This module contains a python script that interacts with the REST api.
+The script exports a give employee data into a CSV foramt"""
+from requests import get
+from sys import argv
+from csv import writer, QUOTE_ALL
+
+
+if __name__ == "__main__":
+    user_info = get('https://jsonplaceholder.typicode.com/users/{}'.
+                    format(argv[1])).json()
+    user_todos = get('https://jsonplaceholder.typicode.com/todos/',
+                     params={"userId": argv[1]}).json()
+    for task in user_todos:
+        USER_ID = str(user_info.get('id'))
+        USERNAME = user_info.get('username')
+        TASK_COMPLETED_STATUS = task.get('completed')
+        TASK_TITLE = task.get('title')
+
+        info_list = [USER_ID, USERNAME, TASK_COMPLETED_STATUS,
+                     TASK_TITLE]
+        file_name = USER_ID + '.csv'
+
+        with open(file_name, "a") as f:
+            w = writer(f, quoting=QUOTE_ALL)
+            w.writerow(info_list)
