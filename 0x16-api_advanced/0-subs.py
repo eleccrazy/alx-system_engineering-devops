80a6d7fd1c19bc691e0df1221ba27c7f7b476473
Gizachew Bayness <eleccrazy24@gmail.com>
2022-11-29 11:00:54 +0300

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This python module contains a script that interacts with the Reddit API"""
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """Queries a reddit api and returns the number of subscribers for
+    a given subredit"""
+    header = {"User-Agent": "0x16.api.advanced.project:eleccrazy"}
+    res = get("https://www.reddit.com/r/{}/about.json".format(subreddit),
+              headers=header)
+    code = res.status_code
+    return 0 if code == 404 else res.json().get("data").get("subscribers")
