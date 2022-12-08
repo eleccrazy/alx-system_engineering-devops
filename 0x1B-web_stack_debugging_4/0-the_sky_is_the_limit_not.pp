efff8eac7a0953ffa8338ab45815d65c6edcf712
Gizachew Bayness <eleccrazy24@gmail.com>
2022-12-08 17:08:48 +0300

    Fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # Fixing Nginx server
-exec { 'fix--for-nginx':
+exec { 'fix':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 } ->
-exec { 'nginx-restart':
+exec { 'restart-service':
   command => 'nginx restart',
   path    => '/etc/init.d/'
 }
