5e5460f85e84aaa8d7bcde25aff06bf010f97c49
Gizachew Bayness <eleccrazy24@gmail.com>
2022-12-08 17:07:59 +0300

    task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,9 @@
+#Change Os Configuration
+exec { 'inc-user-limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+exec { 'inc-user-soft-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
